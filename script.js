b07121a782fc06b43fd5d19997bee0db86986054
April Harris <54374224+aprilharris@users.noreply.github.com>
2023-02-16 19:29:32 +0000

    updated script.js  w/nonArray solution

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 let targetNum = Math.floor(Math.random()*100+1);
-console.log(targetNum);
+console.log(`targetNum: ${targetNum}`);
 
-let guesses = new Array();
+
 
 
 /* optional: uncomment the below code if you want to execute function when the user hits the ENTER key */
-// document.addEventListener('keypress', ({key}) => key === 'Enter' && checkValue());
+/* document.addEventListener('keypress', ({key}) => key === 'Enter' && checkValue());*/
 
 function checkValue(){
    let result = parseInt($(".form-control").val());
 
-  guesses.push(result);
-  /* take the result and insert it inside <ul class="previousGuesses"..> element as a list element */
-  $(".previousGuesses").append(`<li class="fs-2"> ${result} </li>`);
-
-
-   if ( result > targetNum){
-   
-
+  
+   if ( result > targetNum) {
    $("#result").css("color","crimson");
    $("#result").text("Too high!");
  } 
+     
  else if (result < targetNum) {
  
 
@@ -33,11 +28,11 @@ function checkValue(){
    $("#result").css("color","green");
    $("#result").text("You got it!");
 
-   $("#playerFeedback").html(`<h3>Nice job!<br/><br/>You guessed <u class="fw-bold text-primary">${guesses.length}</u> time(s) before you won.<br/><br/>Refresh the page to play again.</h3>`);
+
 
  } else {
   $("#result").css("color","brown");
    $("#result").text("Invalid entry. Please enter an actual number.");
  }
-  console.log(guesses);
+
 };
